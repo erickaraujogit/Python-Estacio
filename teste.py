463f5467c5f34f5c16b8722ee8cbdfa4cbbab293
Erick Araujo <erick.c.araujo@gmail.com>
2025-06-01 18:03:46 -0300

    Terminei

--- teste.py
+++ teste.py
@@ -1,1 +1,8 @@
 #testando
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+import time
+
